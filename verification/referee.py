3d36bb429b259678b07971944d93387b6743e14a
Martinus <31242120+TinusTrotyl@users.noreply.github.com>
2017-12-05 20:34:00 +0100

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,8 +38,8 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "sum_two",
-            "js": "sumTwo"
+            "python": "strawberryfield",
+            "js": "strawberryfield"
         },
         cover_code={
             'python-3': cover_codes.unwrap_args,
